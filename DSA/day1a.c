01753d62cc37b5043156a1a8d37ad343a5a86e99
niteshjangid007 <nitesh242007@gmail.com>
2026-07-02 22:31:44 +0530

    1st day

---
+++ DSA/day1a.c
@@ -0,0 +1,16 @@
+#include<schannel.h>
+int main(){
+    int number[6]={10,20,40,50,60};
+     int current_size=5;
+    int target_index=2;
+    int new_value=30;
+    for(int i=current_size; i>target_index; i--){
+        number[i]=number[i-1];
+    }
+    number[target_index]=new_value;
+    current_size++;
+    printf("after insertion: ");
+    for(int i=0;i<current_size;i++){
+        printf("%d ",number[i]);
+    }
+}
